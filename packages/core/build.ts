TryEverything920609/remix

commit d79478cba1536bfe1cd2fc92866898d72a2a1d03

Stop purging require.cache in the dev server
Also:

- Stop re-evaluating `remix.config.js` in development. Instead, use
  something like nodemon to reload the server when files change.
- Take server "mode" as an explicit arg instead of relying on
  `process.env.NODE_ENV`
- Define `fetch` globals in `@remix-run/express` instead of
  `@remix-run/core`. `core` should be able to assume they are already
  present, just like app code does.

packages/core/build.ts
@@ -1,3 +1,4 @@
+import fs from "fs";
 import path from "path";
 import type { Location } from "history";
 import type { ComponentType } from "react";
@@ -72,19 +73,25 @@ export interface MetaFunction {
   }): { [name: string]: string };
 }
 
+function readJson(file: string) {
+  return JSON.parse(fs.readFileSync(file, "utf-8"));
+}
+
 /**
  * Reads the browser manifest from the build on the filesystem.
  */
 export function getAssetManifest(dir: string): AssetManifest {
-  let file = path.resolve(dir, AssetManifestFilename);
-  return require(file);
+  return readJson(path.resolve(dir, AssetManifestFilename));
 }
 
 /**
  * Reads the server manifest from the build on the filesystem.
  */
 export function getServerManifest(dir: string): ServerManifest {
-  let file = path.resolve(dir, ServerManifestFilename);
+  return readJson(path.resolve(dir, ServerManifestFilename));
+}
+
+function loadModule(file: string) {
   return require(file);
 }
 
@@ -92,24 +99,26 @@ export function getServerManifest(dir: string): ServerManifest {
  * Gets the serve entry module from the build on the filesystem.
  */
 export function getServerEntryModule(
-  dir: string,
+  buildDir: string,
   manifest: ServerManifest
 ): ServerEntryModule {
-  let file = path.resolve(dir, manifest.entries["entry-server"].file);
-  return require(file);
+  return loadModule(
+    path.resolve(buildDir, manifest.entries["entry-server"].file)
+  );
 }
 
 /**
  * Gets all route modules from the build on the filesystem.
  */
 export function getRouteModules(
-  dir: string,
+  buildDir: string,
   routeManifest: RemixConfig["routeManifest"],
   manifest: ServerManifest
 ): RouteModules {
   return Object.keys(routeManifest).reduce((routeModules, routeId) => {
-    let file = path.join(dir, manifest.entries[routeId].file);
-    routeModules[routeId] = require(file);
+    routeModules[routeId] = loadModule(
+      path.join(buildDir, manifest.entries[routeId].file)
+    );
     return routeModules;
   }, {} as RouteModules);
 }

packages/core/server.ts
@@ -1,5 +1,3 @@
-import { URL } from "url";
-
 import type { AssetManifest } from "./build";
 import {
   getAssetManifest,
@@ -11,7 +9,6 @@ import {
 import { getCacheDir } from "./cache";
 import { writeDevServerBuild } from "./compiler";
 import type { RemixConfig } from "./config";
-import { readConfig } from "./config";
 import type { AppLoadContext, AppLoadResult } from "./data";
 import { loadGlobalData, loadRouteData } from "./data";
 import type { EntryManifest, ServerHandoff } from "./entry";
@@ -24,74 +21,63 @@ import {
 } from "./entry";
 import type { Request } from "./fetch";
 import { Headers, Response } from "./fetch";
-import { setupGlobalFetch } from "./globalFetch";
 import type { ConfigRouteObject, ConfigRouteMatch } from "./match";
 import { matchRoutes } from "./match";
-import { purgeRequireCache } from "./requireCache";
 import { json, jsonError } from "./responseHelpers";
 import type { RouteManifest } from "./routes";
 import { oneYear } from "./seconds";
 
-const PROD = process.env.NODE_ENV === "production";
-const TEST = process.env.NODE_ENV === "test";
-const DEV = !(PROD || TEST);
-
-function getConfig(remixRoot?: string): Promise<RemixConfig> {
-  return readConfig(remixRoot).then(config => {
-    setupGlobalFetch(config.rootDirectory);
-    return config;
-  });
+/**
+ * The mode to use when running the server.
+ */
+export enum ServerMode {
+  Development = "development",
+  Production = "production",
+  Test = "test"
 }
 
 /**
  * The main request handler for a Remix server. This handler runs in the context
  * of a cloud provider's server (e.g. Express on Firebase) or locally via their
  * dev tools.
- *
- * The server picks `development` or `production` mode based on the value of
- * `process.env.NODE_ENV`. In production, the server reads the build from disk.
- * In development, it re-evaluates the config and all app modules on every
- * request and dynamically generates the build for only the modules needed to
- * serve it.
  */
 export interface RequestHandler {
   (request: Request, loadContext?: AppLoadContext): Promise<Response>;
 }
 
 /**
- * Creates a HTTP request handler.
+ * Creates a handler (aka "server") that serves HTTP requests from the app in the
+ * given `remixRoot`.
+ *
+ * In production mode, the server reads the build from disk. In development, it
+ * dynamically generates the build at request time for only the modules needed
+ * to serve that request.
  */
-export function createRequestHandler(remixRoot?: string): RequestHandler {
-  let configPromise = getConfig(remixRoot);
-
-  return async (req, loadContext = {}) => {
-    if (DEV) {
-      let config = await configPromise;
-      purgeRequireCache(config.rootDirectory);
-      configPromise = getConfig(remixRoot);
-    }
-
-    let config = await configPromise;
-    let url = new URL(req.url);
+export function createRequestHandler(
+  remixConfig: RemixConfig,
+  serverMode: string = ServerMode.Development
+): RequestHandler {
+  return async (request, loadContext = {}) => {
+    let url = new URL(request.url);
 
     if (url.pathname.startsWith("/_remix/data")) {
-      return handleDataRequest(config, req, loadContext);
+      return handleDataRequest(remixConfig, request, loadContext);
     }
 
     if (url.pathname.startsWith("/_remix/manifest")) {
-      return handleManifestRequest(config, req);
+      return handleManifestRequest(remixConfig, request, serverMode);
     }
 
-    return handleHtmlRequest(config, req, loadContext);
+    return handleHtmlRequest(remixConfig, request, loadContext, serverMode);
   };
 }
 
 async function handleDataRequest(
-  config: RemixConfig,
-  req: Request,
+  remixConfig: RemixConfig,
+  request: Request,
   loadContext: AppLoadContext
 ): Promise<Response> {
-  let searchParams = new URL(req.url).searchParams;
+  let searchParams = new URL(request.url).searchParams;
   let urlParam = searchParams.get("url");
   let routeId = searchParams.get("id");
   let params = JSON.parse(searchParams.get("params") || "{}");
@@ -105,7 +91,7 @@ async function handleDataRequest(
 
   let url = new URL(urlParam);
   let loadResult = await loadRouteData(
-    config,
+    remixConfig,
     routeId,
     params,
     loadContext,
@@ -119,8 +105,12 @@ async function handleDataRequest(
   return loadResult;
 }
 
-async function handleManifestRequest(config: RemixConfig, req: Request) {
-  let searchParams = new URL(req.url).searchParams;
+async function handleManifestRequest(
+  remixConfig: RemixConfig,
+  request: Request,
+  serverMode: string
+) {
+  let searchParams = new URL(request.url).searchParams;
   let urlParam = searchParams.get("url");
 
   if (!urlParam) {
@@ -128,18 +118,18 @@ async function handleManifestRequest(config: RemixConfig, req: Request) {
   }
 
   let url = new URL(urlParam);
-  let matches = matchRoutes(config.routes, url.pathname);
+  let matches = matchRoutes(remixConfig.routes, url.pathname);
 
   if (!matches) {
     return jsonError(`No routes matched path "${url.pathname}"`, 404);
   }
 
   let assetManifest: AssetManifest;
-  if (DEV) {
-    adjustDevConfig(config, matches);
+  if (serverMode === ServerMode.Development) {
+    adjustDevConfig(remixConfig, matches);
 
     try {
-      assetManifest = await getDevAssetManifest(config.publicPath);
+      assetManifest = await getDevAssetManifest(remixConfig.publicPath);
     } catch (error) {
       console.error(error);
       console.error(
@@ -149,7 +139,7 @@ async function handleManifestRequest(config: RemixConfig, req: Request) {
       return jsonError(`Unable to fetch asset manifest`, 500);
     }
   } else {
-    assetManifest = getAssetManifest(config.serverBuildDirectory);
+    assetManifest = getAssetManifest(remixConfig.serverBuildDirectory);
   }
 
   // Get the manifest for only the matched routes.
@@ -174,17 +164,18 @@ async function handleManifestRequest(config: RemixConfig, req: Request) {
 }
 
 async function handleHtmlRequest(
-  config: RemixConfig,
-  req: Request,
-  loadContext: AppLoadContext
+  remixConfig: RemixConfig,
+  request: Request,
+  loadContext: AppLoadContext,
+  serverMode: string
 ): Promise<Response> {
-  let url = new URL(req.url);
+  let url = new URL(request.url);
 
   let statusCode = 200;
-  let matches = matchRoutes(config.routes, url.pathname);
+  let matches = matchRoutes(remixConfig.routes, url.pathname);
 
   function handleDataLoadError(error: any) {
-    if (!TEST) {
+    if (serverMode !== ServerMode.Test) {
       console.error(error);
     }
 
@@ -218,9 +209,9 @@ async function handleHtmlRequest(
   }
 
   // Run all data loaders in parallel and await them individually below.
-  let globalLoadResultPromise = loadGlobalData(config, loadContext, url);
+  let globalLoadResultPromise = loadGlobalData(remixConfig, loadContext, url);
   let routeLoadResultPromises = matches.map(match =>
-    loadRouteData(config, match.route.id, match.params, loadContext, url)
+    loadRouteData(remixConfig, match.route.id, match.params, loadContext, url)
   );
 
   let globalLoadResult: AppLoadResult = null;
@@ -266,22 +257,22 @@ async function handleHtmlRequest(
 
   let serverBuildDirectory: string;
   let assetManifest: AssetManifest;
-  if (DEV) {
-    adjustDevConfig(config, matches);
+  if (serverMode === ServerMode.Development) {
+    adjustDevConfig(remixConfig, matches);
 
-    serverBuildDirectory = getCacheDir(config.rootDirectory, "build");
+    serverBuildDirectory = getCacheDir(remixConfig.rootDirectory, "build");
 
-    await writeDevServerBuild(config, serverBuildDirectory);
+    await writeDevServerBuild(remixConfig, serverBuildDirectory);
 
     try {
-      assetManifest = await getDevAssetManifest(config.publicPath);
+      assetManifest = await getDevAssetManifest(remixConfig.publicPath);
     } catch (error) {
       // The dev server is not running.
       // TODO: Show a nice error page.
       throw error;
     }
   } else {
-    serverBuildDirectory = config.serverBuildDirectory;
+    serverBuildDirectory = remixConfig.serverBuildDirectory;
     assetManifest = getAssetManifest(serverBuildDirectory);
   }
 
@@ -292,7 +283,7 @@ async function handleHtmlRequest(
   );
   let routeModules = getRouteModules(
     serverBuildDirectory,
-    config.routeManifest,
+    remixConfig.routeManifest,
     serverManifest
   );
 
@@ -319,7 +310,7 @@ async function handleHtmlRequest(
     globalData,
     manifest: entryManifest,
     matches: entryMatches,
-    publicPath: config.publicPath,
+    publicPath: remixConfig.publicPath,
     routeData
   };
   let serverEntryContext = {
@@ -359,7 +350,7 @@ async function handleHtmlRequest(
   }, new Headers());
 
   return serverEntryModule.default(
-    req,
+    request,
     statusCode,
     headers,
     serverEntryContext

packages/core/data.ts
@@ -1,5 +1,4 @@
 import path from "path";
-import { URL } from "url";
 import type { Params } from "react-router";
 
 import type { RemixConfig } from "./config";

packages/core/globalFetch.ts
@@ -1,15 +1,0 @@
-// import { getCacheDir } from "./cache";
-import { fetch } from "./fetch";
-
-export function setupGlobalFetch(_rootDirectory: string) {
-  // TODO: Figure out why the types don't match here...
-  // @ts-ignore
-  global.fetch = fetch.defaults({
-    // TODO: Figure out why this doesn't work on 304's returned from the
-    // swapi...
-    // cacheManager: getCacheDir(rootDirectory, "fetch"),
-    // Don't decode responses by default. This lets people return `fetch`
-    // results directly from their loaders w/out changing Content-Encoding.
-    compress: false
-  });
-}

packages/core/globals.ts
@@ -1,10 +1,0 @@
-import { Headers, Request, Response, fetch } from "./fetch";
-
-declare module global {
-  export { Headers, Request, Response, fetch };
-}
-
-global.Headers = Headers;
-global.Request = Request;
-global.Response = Response;
-global.fetch = fetch;

packages/core/index.ts
@@ -1,5 +1,3 @@
-import "./globals";
-
 export type {
   AssetManifest,
   HeadersFunction,
@@ -56,4 +54,4 @@ export {
 export type { RouteManifest, DefineRoute, DefineRoutes } from "./routes";
 
 export type { RequestHandler } from "./server";
-export { createRequestHandler } from "./server";
+export { ServerMode, createRequestHandler } from "./server";

packages/core/requireCache.ts
@@ -1,20 +1,24 @@
-const path = require("path");
-
 /**
  * Purges all entries that begin with the given prefix from node's internal
  * require cache. In Remix, the following files are loaded via CommonJS require:
  *
- * - /app/remix.config.js
- * - /app/src/routes/*
- * - /app/loaders/*
+ * - /remix.config.js
+ * - <serverBuildDirectory>/entry-server.js
+ * - <serverBuildDirectory>/routes/*
+ * - <loadersDirectory>/*
  *
- * This function is useful for clearing all of these from the require cache in
- * dev mode.
+ * This function is useful for clearing all of these from the require cache when
+ * running the build in watch mode.
  */
-export function purgeRequireCache(prefix: string): void {
-  let nodeModules = path.join(prefix, "node_modules");
+export function purgeRequireCache(
+  prefix: string,
+  includeNodeModules = false
+): void {
   for (let key of Object.keys(require.cache)) {
-    if (key.startsWith(prefix) && !key.startsWith(nodeModules)) {
+    if (
+      key.startsWith(prefix) &&
+      (includeNodeModules || !/\bnode_modules\b/.test(key))
+    ) {
       delete require.cache[key];
     }
   }

packages/express/fetchGlobals.ts
@@ -1,0 +1,16 @@
+import { Headers, Request, Response, fetch } from "@remix-run/core";
+
+declare module global {
+  export { Headers, Request, Response, fetch };
+}
+
+global.Headers = Headers;
+global.Request = Request;
+global.Response = Response;
+
+global.fetch = fetch.defaults({
+  // Do not decode responses by default. This lets people return `fetch()`
+  // directly from a loader w/out changing the Content-Encoding of the
+  // response, which is nice.
+  compress: false
+});

commit a1b4c3af6c28f2243e53864fd5f6187464bd9ecb

Fix regression introduced in d79478cb

packages/core/build.ts
@@ -6,7 +6,6 @@ import type { Params } from "react-router";
 
 import type { EntryContext, RouteData } from "./entry";
 import type { Headers, HeadersInit, Request, Response } from "./fetch";
-import { fetch } from "./fetch";
 import type { BuildManifest } from "./rollup/manifest";
 import invariant from "./invariant";
 
@@ -132,14 +131,4 @@ export function getRouteModules(
 
     return routeModules;
   }, {} as RouteModules);
-}
-
-/**
- * Fetches the asset manifest from the asset server.
- */
-export async function getDevAssetManifest(
-  remixRunOrigin: string
-): Promise<AssetManifest> {
-  let res = await fetch(remixRunOrigin + AssetManifestFilename);
-  return res.json();
 }

packages/core/server.ts
@@ -1,10 +1,10 @@
 import type { AssetManifest } from "./build";
 import {
+  AssetManifestFilename,
   getAssetManifest,
   getServerManifest,
   getServerEntryModule,
-  getRouteModules,
-  getDevAssetManifest
+  getRouteModules
 } from "./build";
 import { getCacheDir } from "./cache";
 import { writeDevServerBuild } from "./compiler";
@@ -20,7 +20,7 @@ import {
   createServerHandoffString
 } from "./entry";
 import type { Request } from "./fetch";
-import { Headers, Response } from "./fetch";
+import { Headers, Response, fetch } from "./fetch";
 import type { ConfigRouteObject, ConfigRouteMatch } from "./match";
 import { matchRoutes } from "./match";
 import { json, jsonError } from "./responseHelpers";
@@ -126,14 +126,13 @@ async function handleManifestRequest(
 
   let assetManifest: AssetManifest;
   if (serverMode === ServerMode.Development) {
-    try {
-      assetManifest = await getDevAssetManifest(remixConfig.publicPath);
-    } catch (error) {
-      console.error(error);
-      console.error(
-        `Unable to fetch the asset manifest. Are you running \`remix run\`?`
-      );
+    let devAssetManifestPromise = getDevAssetManifest(
+      getDevPublicPath(remixConfig)
+    );
 
+    try {
+      assetManifest = await devAssetManifestPromise;
+    } catch (error) {
       return jsonError(`Unable to fetch asset manifest`, 500);
     }
   } else {
@@ -262,23 +261,29 @@ async function handleHtmlRequest(
     statusCode = notOkResult.status;
   }
 
+  let publicPath: string;
   let serverBuildDirectory: string;
   let assetManifest: AssetManifest;
   if (serverMode === ServerMode.Development) {
-    adjustDevConfig(remixConfig, matches);
-
+    publicPath = getDevPublicPath(remixConfig);
     serverBuildDirectory = getCacheDir(remixConfig.rootDirectory, "build");
 
-    await writeDevServerBuild(remixConfig, serverBuildDirectory);
+    let devAssetManifestPromise = getDevAssetManifest(publicPath);
+    let devServerBuildPromise = writeDevServerBuild(
+      getDevConfigForMatches(remixConfig, matches),
+      serverBuildDirectory
+    );
 
     try {
-      assetManifest = await getDevAssetManifest(remixConfig.publicPath);
+      assetManifest = await devAssetManifestPromise;
     } catch (error) {
-      // The dev server is not running.
       // TODO: Show a nice error page.
       throw error;
     }
+
+    await devServerBuildPromise;
   } else {
+    publicPath = remixConfig.publicPath;
     serverBuildDirectory = remixConfig.serverBuildDirectory;
     assetManifest = getAssetManifest(serverBuildDirectory);
   }
@@ -317,7 +322,7 @@ async function handleHtmlRequest(
     globalData,
     manifest: entryManifest,
     matches: entryMatches,
-    publicPath: remixConfig.publicPath,
+    publicPath: publicPath,
     routeData
   };
   let serverEntryContext = {
@@ -364,24 +369,54 @@ async function handleHtmlRequest(
   );
 }
 
-function adjustDevConfig(config: RemixConfig, matches: ConfigRouteMatch[]) {
-  // Modify routes and routeManifest so they contain only the matched routes.
-  // This speeds up the build considerably.
-  config.routes = matches.reduceRight((children, match) => {
-    let route = { ...match.route };
-    if (children.length) route.children = children;
-    return [route];
-  }, [] as ConfigRouteObject[]);
+function getDevConfigForMatches(
+  remixConfig: RemixConfig,
+  matches: ConfigRouteMatch[]
+): RemixConfig {
+  return {
+    ...remixConfig,
 
-  config.routeManifest = matches.reduce((routeManifest, match) => {
-    let { children, ...route } = match.route;
-    routeManifest[route.id] = route;
-    return routeManifest;
-  }, {} as RouteManifest);
+    // Modify routes and routeManifest so they contain only the matched routes.
+    // This speeds up the build considerably.
+    routes: matches.reduceRight((children, match) => {
+      let route = { ...match.route };
+      if (children.length) route.children = children;
+      return [route];
+    }, [] as ConfigRouteObject[]),
 
-  // Modify publicPath to point to the dev server.
-  config.publicPath =
-    process.env.REMIX_RUN_ORIGIN || `http://localhost:${config.devServerPort}/`;
+    routeManifest: matches.reduce((routeManifest, match) => {
+      let { children, ...route } = match.route;
+      routeManifest[route.id] = route;
+      return routeManifest;
+    }, {} as RouteManifest)
+  };
+}
+
+function getDevPublicPath(remixConfig: RemixConfig): string {
+  return addTrailingSlash(
+    process.env.REMIX_RUN_ORIGIN ||
+      `http://localhost:${remixConfig.devServerPort}/`
+  );
+}
+
+function addTrailingSlash(path: string): string {
+  return path.endsWith("/") ? path : path + "/";
+}
+
+export async function getDevAssetManifest(
+  remixRunOrigin: string
+): Promise<AssetManifest> {
+  try {
+    let res = await fetch(remixRunOrigin + AssetManifestFilename);
+    return res.json();
+  } catch (error) {
+    console.error(error);
+    console.error(
+      `Unable to fetch the asset manifest. Are you running \`remix run\`?`
+    );
+
+    throw error;
+  }
 }
 
 function getPartialEntries<T = any>(